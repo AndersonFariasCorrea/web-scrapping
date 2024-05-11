68e991ed26fd98e0d758d889f6ae3ac44f36b4b5
Anderson Correa <deidaraa2002@gmail.com>
2024-05-11 17:26:04 -0300

    anderson fix base

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
     console.log("Hello world!");
 });
+
+
+function get(fn, method, data) {
+    return new Promise(function(resolve, reject) {
+        data = data ?? '';
+        $.ajax({
+            url: `${fn}`,
+            method: method,
+            data: data,
+            dataType: 'json'
+        }).done(function(result) {
+            if (Object.keys(result).length > 0) {
+                resolve(result);
+            } else {
+                resolve([]);
+            }
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.error('An error occurred, status:', textStatus);
+            console.error('An error occurred, jqXHR:', jqXHR);
+            reject([]);
+        });
+    }).catch(function(error) {
+        console.error('Error or empty result:', error);
+        return [];
+    });
+}
