e3d958151e2fffde71f0a8da5831c6afacf88c0d
Anderson Correa <deidaraa2002@gmail.com>
2024-05-11 08:48:28 -0300

    anderson PEP 8: E302

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 def main():
     web = classes.web_scrappy.WebSearch()
