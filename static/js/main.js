aacc2b443252f3e233ec3c8d2ae47dce6d218975
daniel <danielmac789@gmail.com>
2024-05-20 18:47:56 -0300

    Daniel add index template

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,60 @@
-$(document).ready(() => {
-    console.log("Hello world!");
+$(document).ready(function() {
+    $("#searchButton").click(function() {
+        var selectedHardware = $("#hardwareSelect").val();
+        var selectedSites = [];
+
+        if ($("#kabumCheckbox").is(":checked")) {
+            selectedSites.push("Kabum");
+        }
+        if ($("#pichauCheckbox").is(":checked")) {
+            selectedSites.push("Pichau");
+        }
+
+        if (selectedHardware === null || selectedSites.length === 0) {
+            alert("Por favor, selecione um hardware e marque pelo menos um site.");
+            return;
+        }
+
+        $.getJSON("busca.json", function(data) {
+            $("#hardwareInfoBody").empty();
+
+            selectedSites.forEach(function(site) {
+                if (data[site] && data[site].itens) {
+                    data[site].itens.forEach(function(item) {
+                        $("#hardwareInfoBody").append(
+                            "<tr>" +
+                            "<td>" + item.nome_item + "</td>" +
+                            "<td>" + site + "</td>" +
+                            "<td>" + item.valor_item + "</td>" +
+                            "<td>" + item.memoria_tamanho + "</td>" +
+                            "<td>" + item.memoria_tipo + "</td>" +
+                            "<td><a href='" + item.link + "' target='_blank'>Ver produto</a></td>" +
+                            "</tr>"
+                        );
+                    });
+                } else {
+                    alert("Erro ao buscar dados do site " + site);
+                }
+            });
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert("Erro: " + textStatus + " - " + errorThrown);
+        });
+    });
+
+    $("#sortByPriceButton").click(function() {
+        var tbody = $("#hardwareInfoBody");
+        var rows = tbody.find("tr").get();
+
+        rows.sort(function(rowA, rowB) {
+            var priceA = parseFloat($(rowA).find("td:eq(2)").text());
+            var priceB = parseFloat($(rowB).find("td:eq(2)").text());
+            return priceA - priceB;
+        });
+
+        $.each(rows, function(index, row) {
+            tbody.append(row);
+        });
+    });
 });
 
 
